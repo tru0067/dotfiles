2b81dc7eb3aec75e052c64d9154dfbb685973436
tru0067@TapLop <33011616+tru0067@users.noreply.github.com>
2021-04-09 09:20:15 +1000

    Focus on hover and Super+Num tweaks

--- gnome/disable_super_num.sh
+++ gnome/disable_super_num.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
+# Allow focus on hover.
+gsettings set org.gnome.desktop.wm.preferences focus-mode 'sloppy'
+# Disable default <Super+Num> app switching.
 gsettings set org.gnome.shell.extensions.dash-to-dock hot-keys false
 for i in $(seq 1 9)
 do
     gsettings set org.gnome.shell.keybindings switch-to-application-${i} []
-    gsettings set org.gnome.shell.extensions.dash-to-dock app-hotkey-${i} []
 done
-
+# TODO: I really could add just about every settings change here, but there
+#       are a whole lot, and I really wanna get a tiling wm set up instead.
+#       For now, these will do as a bare minimum.
