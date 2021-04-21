1ab407626e65cebe8fda8cb40238afb33c9e2fe7
tru0067@TapLop <33011616+tru0067@users.noreply.github.com>
2021-04-21 18:19:53 +1000

    Formatted status script

--- sway/status.sh
+++ sway/status.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 date_str=$(date "+%A %B %-d | %Y-%m-%d %H:%M")
-battery_str=$(upower --show-info $(upower --enumerate | grep "BAT") | grep -E "state|percentage" | awk '{print $2}' | paste -d " " - - | awk '{print $1=="charging" ? $2" ": $2}')
-volume_str=$(amixer -M get Master | grep "Mono:" | awk '{print $6=="[off]" ? $4" ": $4" "}' | tr -d "[]")
+battery_str=$(upower --show-info $(upower --enumerate | grep "BAT") \
+    | grep -E "state|percentage" \
+    | awk 'ORS = " " {print $2}' \
+    | awk '{print $1=="charging" ? $2" ": $2}')
+volume_str=$(amixer -M get Master | grep "Mono:" \
+    | awk '{print $6=="[off]" ? $4" ": $4" "}' \
+    | tr -d "[]")
 echo $volume_str "|" $battery_str "|" $date_str 
