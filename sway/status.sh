720fd5b4891d6c3a2f3212878b6e062f62d7ce1d
tru0067@TapLop <33011616+tru0067@users.noreply.github.com>
2021-04-21 14:48:44 +1000

    Added some icons to status bar

--- sway/status.sh
+++ sway/status.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 date_str=$(date "+%A %B %-d | %Y-%m-%d %H:%M")
-battery_str=$(upower --show-info /org/freedesktop/UPower/devices/battery_BAT0 | grep "percentage" | awk '{print $2}')
-volume_str=$(amixer -M get Master | grep "Mono:" | awk '{print $6=="[off]" ? $4" M": $4" O"}' | tr -d "[]")
+battery_str=$(upower --show-info $(upower --enumerate | grep "BAT") | grep -E "state|percentage" | awk '{print $2}' | paste -d " " - - | awk '{print $1=="charging" ? $2" ": $2}')
+volume_str=$(amixer -M get Master | grep "Mono:" | awk '{print $6=="[off]" ? $4" ": $4" "}' | tr -d "[]")
 echo $volume_str "|" $battery_str "|" $date_str 
