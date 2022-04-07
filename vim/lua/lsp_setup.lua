1b37970844f0c589b017986f8e379265fff08ab7
tru0067@TapLop <33011616+tru0067@users.noreply.github.com>
2022-04-07 22:27:54 +1000

    Add more lsp related mappings

--- vim/lua/lsp_setup.lua
+++ vim/lua/lsp_setup.lua
@@ -1,14 +1,18 @@
--- See `:help lspconfig-server-configurations`.
-
 local on_attach = function()
     vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer = 0})
     vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer = 0})
+    vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, {buffer = 0})
     -- TODO: There's a lot of other possible LSP related actions.
     --       (See `:help vim.lsp.*`.)
     --       Ideally I would like to come up with a way to have a single
     --       keymapping open a floating window with a list of all possible
     --       actions, similar to a right-click context menu.
+    vim.keymap.set("n", "<leader>dj", vim.lsp.diagnostic.goto_next, {buffer = 0})
+    vim.keymap.set("n", "<leader>dk", vim.lsp.diagnostic.goto_prev, {buffer = 0})
 end
+
+-- Language server configurations.
+-- See `:help lspconfig-server-configurations`.
 
 -- Lua.
 local runtime_path = vim.split(package.path, ';')
