5cfb1a38a2c64aa46be1311b0f9c6fc0bce5b815
tru0067@TapLop <33011616+tru0067@users.noreply.github.com>
2022-05-01 13:45:10 +1000

    Change to 'Super-Tab' like mapping for completion

--- vim/lua/lsp_setup.lua
+++ vim/lua/lsp_setup.lua
@@ -1,7 +1,16 @@
+-- Largely default setup. See <https://github.com/hrsh7th/nvim-cmp>,
+-- <https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#super-tab-like-mapping>,
+-- and <https://github.com/neovim/nvim-lspconfig/wiki/Autocompletion>.
 -- `nvim-cmp` setup. {{{
 vim.opt.completeopt = {"menu", "menuone", "noselect"}
 
 local cmp = require"cmp"
+local luasnip = require"luasnip"
+
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
 
 cmp.setup({
     snippet = {
@@ -18,7 +27,26 @@ cmp.setup({
             i = cmp.mapping.abort(),
             c = cmp.mapping.close(),
         }),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            elseif has_words_before() then
+                cmp.complete()
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
     },
     sources = cmp.config.sources({
         { name = 'nvim_lsp' },
