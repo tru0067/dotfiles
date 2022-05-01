b44494ee1981639d4fb16fb48a8f51273cc60032
tru0067@TapLop <33011616+tru0067@users.noreply.github.com>
2022-05-01 13:21:16 +1000

    Migrate fully to init.lua

--- vim/init.lua
+++ vim/init.lua
@@ -1,47 +1,45 @@
-vim.cmd[[
-" This config aims to enhance and extend the default vim experience.
-source ~/.vimrc                        " Source the vim specific settings first.
+vim.cmd"source ~/.vimrc"
 
-set rtp+=~/dotfiles/vim
-set rtp+=~/dotfiles/vim/after
+vim.opt.rtp:append"~/dotfiles/vim"
+vim.opt.rtp:append"~/dotfiles/vim/after"
 
-" Colorscheme.
-set termguicolors                      " Set the terminal highlighting to use full 24 bit colors.
-colorscheme verdurous-darker
+-- Colorscheme.
+vim.opt.termguicolors = true
+vim.cmd"colorscheme verdurous-darker"
 
-" Statusline.
-source ~/dotfiles/vim/statusline.vim
-set laststatus=3
+-- Statusline.
+vim.cmd"source ~/dotfiles/vim/statusline.vim"
+vim.opt.laststatus = 3
 
-" Bindings.
-" Let Ctrl-W delete the previous WORD.
-inoremap <C-W> <C-\><C-O>dB
-" Let Ctrl-Backspace delete the previous word.
-inoremap <C-H> <C-\><C-O>db
-" Let Ctrl-Delete delete the next word.
-inoremap <C-Del> <C-\><C-O>dw
-" Let Ctrl-J/Ctrl-K move the selected region down/up.
-vnoremap <C-J> :m '>+1<CR>gv=gv
-vnoremap <C-K> :m '<-2<CR>gv=gv
-" Correct the last spelling error.
-inoremap <C-L> <C-G>u<ESC>[s1z=`]a<C-G>u
-nnoremap <C-L> m`[s1z=``
-let mapleader=" "
-" Open file explorer.
-nnoremap <leader>e :Lex<CR>
+-- Bindings.
+-- Let Ctrl-W delete the previous WORD.
+vim.keymap.set("i", "<C-W>", "<C-\\><C-O>dB")
+-- Let Ctrl-Backspace delete the previous word.
+vim.keymap.set("i", "<C-H>", "<C-\\><C-O>db")
+-- Let Ctrl-Delete delete the next word.
+vim.keymap.set("i", "<C-Del>", "<C-\\><C-O>dw")
+-- Correct the last spelling error.
+vim.keymap.set("i", "<C-L>", "<C-G>u<ESC>[s1z=`]a<C-G>u")
+vim.keymap.set("n", "<C-L>", "m`[s1z=``")
+vim.g.mapleader = " "
+-- Open file explorer.
+vim.keymap.set("n", "<leader>e", ":Lex<CR>")
 
-" Commands.
-" See unsaved changes.
-command Changes :w !diff % -
+-- Commands.
+-- See unsaved changes.
+vim.api.nvim_create_user_command("Changes", ":w !diff % -", {})
 
-" Autocommands.
-" Highlight yanks.
-augroup highlight_yank
-    autocmd!
-    au TextYankPost * silent! lua vim.highlight.on_yank{higroup="Visual", timeout=120}
-augroup END
-]]
+-- Autocommands.
+-- Highlight yanks.
+local augroup_highlight_yank = vim.api.nvim_create_augroup("HighlightYank", {})
+vim.api.nvim_create_autocmd("TextYankPost", {
+    group = augroup_highlight_yank,
+    callback = function()
+        vim.highlight.on_yank{higroup="Visual", timeout=120}
+    end,
+})
 
+-- Plugins.
 require"packer".startup(function(use)
     use{"wbthomason/packer.nvim"}
     -- Neovim core plugins.
@@ -69,4 +67,5 @@ require"nvim-treesitter.configs".setup{
 
 require"colorizer".setup({"*"}, {RGB = false, names = false})
 
+-- Other config files.
 require"lsp_setup"
