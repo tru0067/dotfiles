7c916e362bd1b2589771f1243294a5b300e8552e
tru0067@TapLop <33011616+tru0067@users.noreply.github.com>
2025-06-29 23:29:49 +1000

    Update to use `nvim_get_option_value`

--- vim/lua/scrolloff.lua
+++ vim/lua/scrolloff.lua
@@ -1,14 +1,14 @@
 local function set_scrolloff()
     -- Vertical (fractional) scrolloff.
     local FRACTION = 0.2
-    local lines = vim.api.nvim_eval"winheight(0)"
+    local lines = vim.api.nvim_eval("winheight(0)")
     local scrolloff = math.floor(FRACTION * lines)
     vim.opt.scrolloff = scrolloff
 
     -- Horizontal scrolloff.
     local DEFAULT = 8
-    local cols = vim.api.nvim_eval"winwidth(0)"
-    local tw = vim.api.nvim_get_option"textwidth"
+    local cols = vim.api.nvim_eval("winwidth(0)")
+    local tw = vim.api.nvim_get_option_value("textwidth", {})
     if cols > tw then
         vim.opt.sidescrolloff = 1
     else
